7fbefed0c511e9e290e9b3760f2f7db7ce916a6e
ryusue1222 <76861277+ryusue1222@users.noreply.github.com>
2021-03-28 18:33:13 +0900

    正規表現の修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    @kanji = /\A[ぁ-んァ-ン一-龥々]+\z/
+    @kanji = /\A[ぁ-んァ-ン一-龥々ー]+\z/
     @kana = /\A[ァ-ヶー－]+\z/
     validates :nickname, length: { maximum: 40 }
     validates :last_name, format: {with: @kanji}
